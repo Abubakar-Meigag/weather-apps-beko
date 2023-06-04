337c7dbf59f5e6030837d8e49df9ae00b732ebc6
Abubakar Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2023-06-04 14:46:55 +0100

    Add files via upload

---
+++ Weather.js
@@ -0,0 +1,90 @@
+// window.addEventListener('load', () => {
+//     let long;
+//     let lat;
+//     let apiKey = "3dab0660ed2744c8ba347bef48799d40";
+
+//     let temperatureDescription = document.querySelector(".temperature-description");
+//     let temperatureDegree = document.querySelector(".temperature-degree");
+//     let locationTimezone = document.querySelector(".location-timezone");
+//     let weatherIcon = document.querySelector(".weather-icon-img")
+
+//     if(navigator.geolocation){
+//         navigator.geolocation.getCurrentPosition(position => {
+//             long = position.coords.longitude;
+//             lat = position.coords.latitude;
+
+//             const api = `https://api.weatherbit.io/v2.0/current?lat=${lat}&  lon=${long}&key=${apiKey}&lang=eng
+//             `;
+
+//             fetch(api)
+//                 .then(response => {
+//                     return response.json()
+//                 })
+//                 .then(data => {
+//                     console.log(data);
+//                     const {timezone, temp, sunrise, sunset} = data.data[0];
+//                     const { description, icon } = data.data[0].weather;
+
+//                     // set DOM Element from the API
+
+//                     temperatureDegree.textContent = Math.floor(temp);
+//                     temperatureDescription.textContent = description;
+//                     locationTimezone.textContent = timezone;
+//                 })
+//         });
+//     }
+// })
+
+// ////////////////////////////////////////////////////////////////
+
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let apiKey = "3dab0660ed2744c8ba347bef48799d40";
+
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let imgDvi = document.querySelector(".abc");
+  //   let weatherIcon = document.querySelector(".weather-icon");
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const api = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${long}&key=${apiKey}&lang=eng`;
+
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          const { timezone, temp, sunrise, sunset } = data.data[0];
+          const { description, icon } = data.data[0].weather;
+          let weatherIcon = document.createElement("img");
+
+          // Set the weather icon class based on the icon code
+          //   weatherIcon.className = "weather-icon fas fa-" + icon;
+
+          // Set DOM Elements from the API
+          temperatureDegree.textContent = Math.floor(temp);
+          temperatureDescription.textContent = description;
+          locationTimezone.textContent = timezone;
+          weatherIcon.src =
+            "https://cdn.weatherbit.io/static/img/icons/t01d.png";
+          console.log(icon);
+          imgDvi.appendChild(weatherIcon)
+        });
+    });
+  }
+});
+
+// generateImageUrl
+// input: icon string example: c02d
+// output: https://cdn.weatherbit.io/static/img/icons/
+//c02d.png
+// https://cdn.weatherbit.io/static/img/icons/t01d.png
